17d0d3ecdd27b796d8582fee1c4a2dec98e68fcf
John Whitson <john.whitson@gmail.com>
2013-06-13 13:06:23 -0600

    Modified MIG common module - Added audio_codec_id to common video variables - Added fourcc list - Changed video_codec to video_codec_description - Changed codec_id to video_codec_id - Changed codec_commercial_name to video_codec_commercial_name

--- lib/mig/modules/common.rb
+++ lib/mig/modules/common.rb
@@ -35,6 +35,7 @@ class MediaInformationGatherer
         ffmpeg = metadata_sources[:ffmpeg] || { }
         mediainfo = metadata_sources[:mediainfo] || { 'section_type_count' => { 'audio' => 0 } }
         mi_video = mediainfo['video'] || { }
+        mi_audio = mediainfo['audio'] || { }
 
         section_type_counts = mediainfo['section_type_counts'] || { }
         audio_track_count = section_type_counts['audio']
@@ -43,16 +44,17 @@ class MediaInformationGatherer
         height = ffmpeg['height']
         width = ffmpeg['width']
 
-        video_codec = determine_video_codec(metadata_sources)
+        video_codec_id = mi_video['Codec ID']
+        video_codec_description = fourcc_codes.fetch(video_codec_id, 'Unknown')
+
         video_system = determine_video_system(height, width, frame_rate)
 
         cv[:aspect_ratio] = ffmpeg['is_widescreen'] ? '16:9' : '4:3'
+        cv[:audio_codec_id] = mi_audio['Codec ID']
         cv[:audio_sample_rate] = ffmpeg['audio_sample_rate']
         cv[:bit_depth] = mi_video['Bit depth']
         cv[:calculated_aspect_ratio] = ffmpeg['calculated_aspect_ratio']
         cv[:chroma_subsampling] = mi_video['Chroma subsampling']
-        cv[:codec_id] = mi_video['Codec ID']
-        cv[:codec_commercial_name] = mi_video['Commercial name']
         cv[:duration] = ffmpeg['duration']
         cv[:frames_per_second] = frame_rate # Video frames per second
         cv[:height] = height
@@ -63,63 +65,119 @@ class MediaInformationGatherer
         cv[:scan_order] = mi_video['Scan order']
         cv[:scan_type] = mi_video['Scan type']
         cv[:timecode] = ffmpeg['timecode']
-        cv[:video_codec] = video_codec ? video_codec_friendly_names.fetch(video_codec, video_codec) : 'unknown'
+        cv[:video_codec_id] = video_codec_id
+        cv[:video_codec_commercial_name] = mi_video['Commercial name']
+        cv[:video_codec_description] = video_codec_description
         cv[:video_system] = video_system
         cv[:width] = width
         cv
       end # common_video_variables
 
-      def video_codec_friendly_names
-        @video_codec_friendly_names ||= {
-          :unknown                  => 'unknown',
-          :dv                       => 'DV',
-          :imx_50                   => 'IMX 50',
-          :motion_jpeg              => 'Motion JPEG',
-          :photo_jpeg               => 'Photo JPEG',
-          :prores_high_quality      => 'Apple ProRes High Quality',
-          :prores_proxy             => 'Apple ProRes Proxy',
-          :prores_standard_quality  => 'Apple ProRes Standard quality',
-          :xdcam_hd                 => 'XDCAM HD',
+      # A hash of fourcc codes
+      # http://www.videolan.org/developers/vlc/src/misc/fourcc.c
+      def fourcc_codes
+        @fourcc_codes ||= {
+          'apcn' => 'Apple ProRes Standard Definition (SD)',
+          'apch' => 'Apple ProRes High Quality (HQ)',
+          'apcs' => 'Apple ProRes LT',
+          'apco' => 'Apple ProRes Proxy',
+          'ap4c' => 'Apple ProRes 4444',
+          'ap4h' => 'Apple ProRes 4444',
+          'xdv1' => 'XDCAM HD 720p30 35Mb/s',
+          'xdv2' => 'XDCAM HD 1080i60 35Mb/s',
+          'xdv3' => 'XDCAM HD 1080i50 35Mb/s',
+          'xdv4' => 'XDCAM HD 720p24 35Mb/s',
+          'xdv5' => 'XDCAM HD 720p25 35Mb/s',
+          'xdv6' => 'XDCAM HD 1080p24 35Mb/s',
+          'xdv7' => 'XDCAM HD 1080p25 35Mb/s',
+          'xdv8' => 'XDCAM HD 1080p30 35Mb/s',
+          'xdv9' => 'XDCAM HD 720p60 35Mb/s',
+          'xdva' => 'XDCAM HD 720p50 35Mb/s',
+          'xdhd' => 'XDCAM HD 540p',
+          'xdh2' => 'XDCAM HD422 540p',
+          'xdvb' => 'XDCAM EX 1080i60 50Mb/s CBR',
+          'xdvc' => 'XDCAM EX 1080i50 50Mb/s CBR',
+          'xdvd' => 'XDCAM EX 1080p24 50Mb/s CBR',
+          'xdve' => 'XDCAM EX 1080p25 50Mb/s CBR',
+          'xdvf' => 'XDCAM EX 1080p30 50Mb/s CBR',
+          'xd54' => 'XDCAM HD422 720p24 50Mb/s CBR',
+          'xd55' => 'XDCAM HD422 720p25 50Mb/s CBR',
+          'xd59' => 'XDCAM HD422 720p60 50Mb/s CBR',
+          'xd5a' => 'XDCAM HD422 720p50 50Mb/s CBR',
+          'xd5b' => 'XDCAM HD422 1080i60 50Mb/s CBR',
+          'xd5c' => 'XDCAM HD422 1080i50 50Mb/s CBR',
+          'xd5d' => 'XDCAM HD422 1080p24 50Mb/s CBR',
+          'xd5e' => 'XDCAM HD422 1080p25 50Mb/s CBR',
+          'xd5f' => 'XDCAM HD422 1080p30 50Mb/s CBR',
+          'dvh2' => 'DV Video 720p24',
+          'dvh3' => 'DV Video 720p25',
+          'dvh4' => 'DV Video 720p30',
+          'dvcp' => 'DV Video PAL',
+          'dvc ' => 'DV Video NTSC',
+          'dvp ' => 'DV Video Pro',
+          'dvpp' => 'DV Video Pro PAL',
+          'dv50' => 'DV Video C Pro 50',
+          'dv5p' => 'DV Video C Pro 50 PAL',
+          'dv5n' => 'DV Video C Pro 50 NTSC',
+          'dv1p' => 'DV Video C Pro 100 PAL',
+          'dv1n' => 'DV Video C Pro 100 NTSC',
+          'dvhp' => 'DV Video C Pro HD 720p',
+          'dvh5' => 'DV Video C Pro HD 1080i50',
+          'dvh6' => 'DV Video C Pro HD 1080i60',
+          'AVdv' => 'AVID DV',
+          'AVd1' => 'MPEG2 I',
+          'mx5n' => 'MPEG2 IMX PAL 625/60 50Mb/s (FCP)"',
+          'mx5p' => 'MPEG2 IMX NTSC 525/60 40Mb/s (FCP',
+          'mx4n' => 'MPEG2 IMX PAL 625/50 40Mb/s (FCP)"',
+          'mx4p' => 'MPEG2 IMX NTSC 525/60 30Mb/s (FCP',
+          'mx3n' => 'MPEG2 IMX NTSC 625/50 30Mb/s (FCP)',
+          'mx3p' => 'HDV 720p30 (MPEG-2 Video)"),',
+          'hdv1' => 'Sony HDV 1080i60 (MPEG-2 ',
+          'hdv2' => 'FCP HDV 1080i50 (MPEG-2 Video)"',
+          'hdv3' => 'HDV 720p24 (MPEG-2 Video)"),',
+          'hdv4' => 'HDV 720p25 (MPEG-2 Video)',
+          'hdv5' => 'HDV 1080p24 (MPEG-2 Video',
+          'hdv6' => 'HDV 1080p25 (MPEG-2 Video)',
+          'hdv7' => 'HDV 1080p30 (MPEG-2 Video)',
+          'hdv8' => 'HDV 720p60 JVC (MPEG-2 Vid',
+          'hdv9' => 'HDV 720p50 (MPEG-2 Video)"),',
+          'hdva' => 'AVC-Intra"),',
+          'avc1' => 'AVC-Intra',
+          'ai5p' => 'AVC-Intra  50M 720p25/50"),',
+          'ai5q' => 'AVC-Intra  50M 1080p25/5',
+          'ai52' => 'AVC-Intra  50M 1080p24/30',
+          'ai53' => 'AVC-Intra  50M 1080i50"),',
+          'ai55' => 'AVC-Intra  50M 1080i60',
+          'ai56' => 'AVC-Intra 100M 720p24/',
+          'ai1p' => 'AVC-Intra 100M 720p25/50"),',
+          'ai1q' => 'AVC-Intra 100M 1080p25/5',
+          'ai12' => 'AVC-Intra 100M 1080p24/30',
+          'ai13' => 'AVC-Intra 100M 1080i50"),',
+          'ai15' => 'AVC-Intra 100M 1080i60',
+          'ai16' => 'AVC-Intra 100M 1080i60',
+          'mpgv' => 'MPEG-2',
+          'mp1v' => 'MPEG-2',
+          'mpeg' => 'MPEG-2',
+          'mpg1' => 'MPEG-2',
+          'mp2v' => 'MPEG-2',
+          'MPEG' => 'MPEG-2',
+          'mpg2' => 'MPEG-2',
+          'MPG2' => 'MPEG-2',
+          'H262' => 'MPEG-2',
+          'mjpg' => 'Motion JPEG',
+          'mJPG' => 'Motion JPEG',
+          'mjpa' => 'Motion JPEG',
+          'JPEG' => 'Motion JPEG',
+          'AVRn' => 'Avid Motion JPEG',
+          'AVRn' => 'Avid Motion JPEG',
+          'AVDJ' => 'Avid Motion JPEG',
+          'ADJV' => 'Avid Motion JPEG',
+          'wvc1' => 'Microsoft VC-1',
+          'vc-1' => 'Microsoft VC-1',
+          'VC-1' => 'Microsoft VC-1',
+          'jpeg' => 'Photo JPEG',
         }
-      end # video_codec_friendly_names
-
-      def determine_video_codec(metadata_sources)
-        video_codec = :unknown
-
-        mediainfo_metadata = metadata_sources[:mediainfo] || { }
-        mi_video = mediainfo_metadata['video'] || { }
-
-        format = mi_video['Format']
-        return video_codec unless format
-        case format
-        when 'ProRes'
-          format_profile = mi_video['Format profile']
-          case format_profile
-          when nil; video_codec = :prores_standard_quality
-          when 'High'; video_codec = :prores_high_quality
-          when 'Proxy'; video_codec = :prores_proxy
-          end
-
-        when 'MPEG Video'
-          commercial_name = mi_video['Commercial name']
-          case commercial_name
-          when 'IMX 50'; video_codec = :imx_50
-          when 'XDCAM HD422'; video_codec = :xdcam_hd
-          end
-        when 'DV'
-          codec_id = mi_video['Codec ID']
-          case codec_id
-          when 'dvc'; video_codec = :dv
-          end
-        when 'JPEG'
-          codec_id = mi_video['Codec ID']
-          case codec_id
-          when 'mjpa'; video_codec = :motion_jpeg
-          when 'jpeg'; video_codec = :photo_jpeg
-          end
-        end
-        video_codec
-      end # determine_video_codec
+      end # fourcc_codes
 
       def determine_video_system(height, width, frame_rate)
         # http://en.wikipedia.org/wiki/Broadcast_television_system
@@ -129,11 +187,14 @@ class MediaInformationGatherer
         # Programmer's Guide to Video Systems - http://lurkertech.com/lg/video-systems/#fields
 
         video_system = 'unknown'
-        return video_system unless height and width and frame_rate and STANDARD_VIDEO_FRAME_RATES.include?(frame_rate)
+
+        return video_system unless height and width and frame_rate
+
+        frame_rate = frame_rate.to_f
+        return video_system unless STANDARD_VIDEO_FRAME_RATES.include?(frame_rate)
 
         height = height.to_i
         width = width.to_i
-        frame_rate = frame_rate.to_f
 
         # The following case statement is based off of - http://images.apple.com/finalcutpro/docs/Apple_ProRes_White_Paper_October_2012.pdf
         case height
