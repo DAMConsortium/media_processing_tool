369755eeadf23ce6a4ded1eb3f76779726c4718d
John Whitson <john.whitson@gmail.com>
2013-07-03 11:42:26 -0600

    Added parse and parse_files methods to the FCP X XML parser

--- lib/final_cut_pro/xml_parser/fcpxml/version_1.rb
+++ lib/final_cut_pro/xml_parser/fcpxml/version_1.rb
@@ -5,6 +5,22 @@ module FinalCutPro
     class FCPXML
       class Version1 < FinalCutPro::XMLParser::Common
 
+        def self.parse(xml, params = {})
+          parser = new(xml)
+          parser.parse(parser.xml_document, params)
+        end # self.parse
+
+        def parse(xml = @xml_document, options = { })
+          @files = parse_files(xml, options)
+          return self
+        end # parse
+
+        def parse_files(xml = @xml_document, options = { })
+          xml.find('//asset').map do |asset_node|
+            hash = xml_node_to_hash(asset_node)
+            hash.merge!({ path_on_file_system: CGI::unescape(URI(hash[:src]).path) })
+          end
+        end # parse_files
 
       end # Version1
     end # FCPXML
